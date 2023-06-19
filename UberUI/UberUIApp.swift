3d6974220db0abeb26f827cc9bc22d4adc20c58b
alialdwahy <aligowi358@gmail.com>
2023-06-19 11:27:11 +0300

    Initial Commit

---
+++ UberUI/UberUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberUIApp.swift
+//  UberUI
+//
+//  Created by ali gowi on 01/12/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct UberUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
